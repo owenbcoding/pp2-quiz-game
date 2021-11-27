a70f2b12c648f9f12ec1c096d320a4481a80ce93
eoghankb <eoghankb@gmail.com>
2021-11-27 22:19:22 +0000

    Add User Stories

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # pp2-quiz-game
 A Javascript Quiz Game.
 
-Introduction
+## Introduction
 Quiz-Game is a site where the user can play a quiz for entertainment which will generate random questions for the user.
 
-Context
+## Context
 
 ( Wireframe from I am responsive goes here)
 
@@ -15,9 +15,15 @@ Quiz-Game: ( Some text here )
 
 Ux
 
-User Stories
+### User Stories
 --------------------
-Wireframe mockups
+- A user will be able to input their data name to login
+- A user will be able to play and choose from multiple to topics to answer
+- A user will get a score that is assigned when answered correctly 
+- A user will gain progress on each topic as they answer correctly
+- A user will be able to see their incorrect answeres in the progress bar 
+
+### Wireframe mockups
 --------------------
 Features
 (images of website go here)
---
+++ index.js
@@ -0,0 +1,139 @@
+// const question = document.querySelector('#question');
+// const choices = Array.from(document.querySelectorAll('.choice-text'));
+// const progressText = document.querySelector('#progressText');
+// const scoreText = document.querySelector('#score');
+// const progressBarFull = document.querySelector('#progressBarFull');
+
+// let currentQuestion = {}
+// let acceptAnsweres = true
+// let score = 0 
+// let questionCounter = 0 
+// let availableQuestions = []
+
+// let questions = [
+//     {
+//         question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, adipisci.',
+//         choice1: 'Test1',
+//         choice2: 'Test2',
+//         choice3: 'Test3',
+//         choice4: 'Test4',
+//         answer: 4,
+//     },
+//     {
+//         question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, adipisci?.',
+//         choice1: 'Test1',
+//         choice2: 'Test2',
+//         choice3: 'Test3',
+//         choice4: 'Test4',
+//         answer: 2,
+//     },
+//     {
+//         question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, adipisci.?',
+//         choice1: 'Test1',
+//         choice2: 'Test2',
+//         choice3: 'Test3',
+//         choice4: 'Test4',
+//         answer: 2,
+//     },
+//     {
+//         question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, adipisci?',
+//         choice1: 'Test1',
+//         choice2: 'Test2',
+//         choice3: 'Test3',
+//         choice4: 'Test4',
+//         answer: 2,
+//     }
+// ]
+
+// let sports = [
+//     {
+//           question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, adipisci.',
+//           choices: ['opt1', 'opt2', 'opt3', 'opt4'],
+//       answer: 4,
+//     },
+//      {
+//           question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, adipisci.',
+//           choices: ['opt1', 'opt2', 'opt3', 'opt4'],
+//       answer: 4,
+//     },
+//      {
+//           question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, adipisci.',
+//           choices: ['opt1', 'opt2', 'opt3', 'opt4'],
+//       answer: 4,
+//     },
+//      {
+//           question: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, adipisci.',
+//           choices: ['opt1', 'opt2', 'opt3', 'opt4'],
+//       answer: 4,
+//     },
+   
+// ],
+
+// array, sports: []
+
+// const SCORE_POINTS = 100 
+// const MAX_QUESTIONS = 4
+
+// startGame = () => {
+//     questionCounter = 0;
+//     score = 0;
+//     availableQuestions = [...questions]
+//     getNewQuestions()
+// }
+
+// getNewQuestion = () => {
+//     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+//         localStorage.setItem('mostRecentScore',score)
+
+//         return window.location.assign('/end.html')
+//     }
+
+//     questionCounter++
+//     progressText.innerHTML = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+//     //progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) = 100}%`
+
+//     const questionsIndex = Math.floor(Math.random() =availableQuestions.length)
+//     currentQuestion = availableQuestions[questionsIndex]
+//     question.innerText = currentQuestion.question
+
+//     choices.forEach(choices => {
+//         const number = choice.dataset['number']
+//         choice.innerText = currentQuestion['choice' + number]
+//     })
+
+//     availableQuestions.splice(questionsIndex, 1)
+
+//     acceptAnsweres = true
+// }
+
+// choices.forEach(choice => {
+//     choice.addEventListener('click', e => {
+//         if(!acceptAnsweres) return
+
+//         acceptAnsweres = false
+//         const selectedChoice = e.target
+//         const selectedAnswer = selectedChoice.dataset['number']
+
+//         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
+//         'incorrect'
+
+//         if(classToApply === 'correct') {
+//             incrementScore(SCORE_POINTS)
+//         }
+
+//         setTimeout(() => {
+//         selectedChoice.parentElement.classList.remove(classToApply)
+//         getNewQuestion()
+
+//         }, 1000)
+//     })
+// })
+// console.log(choices);
+
+// incrementScore = num => {
+//     score += num
+//     scoreText.innerText = score
+// }
+
+// startGame()
+// console.log("Hello");
