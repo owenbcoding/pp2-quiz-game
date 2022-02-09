e771749f31b96319b925f97bc7aedce6bebf2735
eoghankb <eoghankb@gmail.com>
2022-02-09 20:10:09 +0000

    initalized variables in sports.js

--- sports.js
+++ sports.js
@@ -3,3 +3,10 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.getElementById('progressiontText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+
+//initialized variables
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0 
+let questionCounter = 0 
+let availableQuestions = {}
