6a8695a8505313632b059110216447928d4b8060
eoghankb <eoghankb@gmail.com>
2022-02-09 20:08:46 +0000

    Added Consts to sports.js page

---
+++ index.js
@@ -0,0 +1,8 @@
+// function submitName (){
+//     localStorage.setItem('uersname',document.getElementById('username').value);
+//      window.location.href="home.html";
+//  }
+ 
+//  window.onload = function(){
+//      let username = localStorage.getItem('username');
+//  }
---
+++ sports.js
@@ -0,0 +1,5 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.getElementById('progressiontText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
