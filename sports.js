bb643f973a72dc505dfff4046baf116442cddf3c
eoghankb <eoghankb@gmail.com>
2022-02-09 20:28:49 +0000

    Added function when answer is correct get new question

--- sports.js
+++ sports.js
@@ -50,3 +50,38 @@ let questions = [
 //Defining Constants
 const SCORE_POINTS = 100 
 const MAX_QUESTIONS = 4
+
+//Function starts the game when the page has loaded
+let startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+document.addEventListener('DOMContentLoaded', startGame);
+
+//When A questions is answered gets new question
+let getNewQuestion = () => { 
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
+        localStorage.setItem('mostRecentScore', score)
+        localStorage.setItem('lastGame', "sports")
+
+        return window.location.assign('/end.html')
+     }
+        questionCounter++
+        progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+        progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
+
+        const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+        currentQuestion = availableQuestions[questionIndex]
+        question.innerText = currentQuestion.question
+
+        choices.forEach(choice => { 
+            const number = choice.dataset['number']
+            choice.innerText = currentQuestion['choice' + number]
+        })
+
+        availableQuestions.splice(questionIndex, 1)
+
+        acceptingAnswers = true
+}
