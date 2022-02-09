5305a5b7a5f0a7476c70a4959e4dc327a64f3830
eoghankb <eoghankb@gmail.com>
2022-02-09 20:13:16 +0000

    Added place holder questions for sports page

--- sports.js
+++ sports.js
@@ -10,3 +10,39 @@ let acceptingAnswers = true
 let score = 0 
 let questionCounter = 0 
 let availableQuestions = {}
+
+//Setup questions Arry
+let questions = [
+    { 
+        question: 'What is 2 + 2?',
+        choice1: '2',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',   
+        answer: 2,
+    },
+    { 
+        question: 'The tallest building in the world is located in wich city?',
+        choice1: 'Dubai',
+        choice2: 'New York',
+        choice3: 'Shanghai',
+        choice4: 'None of the above',   
+        answer: 1,
+    },
+    { 
+        question: 'What precet of American adules blieve that cholocate milk comes from brown cows?',
+        choice1: '20%',
+        choice2: '18%',
+        choice3: '7%',
+        choice4: '33%',   
+        answer: 3,
+    },
+    { 
+        question: 'Approximately what percent of U.S power outages are caused by squirrels?',
+        choice1: '10-20%',
+        choice2: '5-10%',
+        choice3: '15-20%',
+        choice4: '30-40%',   
+        answer: 1,
+    }
+]
