f0468e6075898ba5bf3c9166c1424fb190ed392b
eoghankb <eoghankb@gmail.com>
2022-02-10 01:08:47 +0000

    Converted questions to an object based on category

--- game.js
+++ game.js
@@ -7,157 +7,146 @@ const progressBarFull = document.getElementById('progressBarFull');
 //initialized variables
 let currentQuestion = {}
 let acceptingAnswers = true
-let score = 0 
-let questionCounter = 0 
+let score = 0
+let questionCounter = 0
 let availableQuestions = {}
 
 //Setup questions Arry
-let questions = [
-     'sports', { 
-        question: 'Sports',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',   
-        answer: 2,
-    },
-    { 
-        question: 'Sports',
-        choice1: 'Dubai',
-        choice2: 'New York',
-        choice3: 'Shanghai',
-        choice4: 'None of the above',   
-        answer: 1,
-    },
-    { 
-        question: 'Sports',
-        choice1: '20%',
-        choice2: '18%',
-        choice3: '7%',
-        choice4: '33%',   
-        answer: 3,
-    },
-    { 
-        question: 'Sports',
-        choice1: '10-20%',
-        choice2: '5-10%',
-        choice3: '15-20%',
-        choice4: '30-40%',   
-        answer: 1,
-    },
-
-    'movies',  { 
-        question: 'movies',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',   
-        answer: 2,
-    },
-    { 
-        question: 'movies',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',   
-        answer: 2,
-    },
-    { 
-        question: 'movies',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',   
-        answer: 2,
-    },
-    { 
-        question: 'movies',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',   
-        answer: 2,
-    },
-
-    "litrature",  { 
-        question: 'litrature',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',   
-        answer: 2,
-    },
-    { 
-        question: 'litrature',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',   
-        answer: 2,
-    },
-    { 
-        question: 'litrature',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',   
-        answer: 2,
-    },
-]
+let questions = {
+    sports: [
+        {
+            question: 'Sports question 1',
+            choice1: '1',
+            choice2: '2',
+            choice3: '3',
+            choice4: '4',
+            answer: 2,
+        },
+        {
+            question: 'Sports question 2',
+            choice1: '1',
+            choice2: '2',
+            choice3: '3',
+            choice4: '4',
+            answer: 2,
+        },
+        {
+            question: 'Sports question 3',
+            choice1: '1',
+            choice2: '2',
+            choice3: '3',
+            choice4: '4',
+            answer: 2,
+        },
+        {
+            question: 'Sports question 4',
+            choice1: '1',
+            choice2: '2',
+            choice3: '3',
+            choice4: '4',
+            answer: 2,
+        },
+    ],
+    movies: [
+        {
+            question: 'movies question 1',
+            choice1: '1',
+            choice2: '2',
+            choice3: '3',
+            choice4: '4',
+            answer: 2,
+        },
+        {
+            question: 'movies question 2',
+            choice1: '1',
+            choice2: '2',
+            choice3: '3',
+            choice4: '4',
+            answer: 2,
+        },
+        {
+            question: 'movies question 3',
+            choice1: '1',
+            choice2: '2',
+            choice3: '3',
+            choice4: '4',
+            answer: 2,
+        },
+        {
+            question: 'movies question 4',
+            choice1: '1',
+            choice2: '2',
+            choice3: '3',
+            choice4: '4',
+            answer: 2,
+        },
+    ]
+}
+console.log(questions.sports)
 
 //Defining Constants
-const SCORE_POINTS = 100 
+const SCORE_POINTS = 100
 const MAX_QUESTIONS = 4
 
 //Function starts the game when the page has loaded
 let startGame = () => {
     questionCounter = 0
     score = 0
-    availableQuestions = [...questions]
+    if (game == 'sports'){
+       availableQuestions = [...questions.sports]
+    } else if (game == 'movies'){
+        availableQuestions = [...questions.movies]
+    } else {
+        availableQuestions = [...questions.litrature]
+    }
     getNewQuestion()
 }
 document.addEventListener('DOMContentLoaded', startGame);
 
 //When A questions is answered gets new question
-let getNewQuestion = () => { 
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
+let getNewQuestion = () => {
+    console.log(availableQuestions[0].question);
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
         localStorage.setItem('lastGame', "sports")
 
         return window.location.assign('/end.html')
-     }
-        questionCounter++
-        progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-        progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
+    }
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
 
-        const questionIndex = Math.floor(Math.random() * availableQuestions.length)
-        currentQuestion = availableQuestions[questionIndex]
-        question.innerText = currentQuestion.question
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    console.log(questionIndex);
+    currentQuestion = availableQuestions[questionIndex]
+    console.log(currentQuestion);
+    question.innerText = currentQuestion.question
+    console.log(currentQuestion.question);
 
-        choices.forEach(choice => { 
-            const number = choice.dataset['number']
-            choice.innerText = currentQuestion['choice' + number]
-        })
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
 
-        availableQuestions.splice(questionIndex, 1)
+    availableQuestions.splice(questionIndex, 1)
 
-        acceptingAnswers = true
+    acceptingAnswers = true
 }
 
 //listens for correct answer
-choices.forEach(choice => { 
+choices.forEach(choice => {
     choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
+        if (!acceptingAnswers) return
 
-        acceptingAnswers = false 
+        acceptingAnswers = false
         const selectedChoice = e.target
         const slectedAnswer = parseInt(selectedChoice.dataset['number']);
         console.log(slectedAnswer, currentQuestion.answer)
-        
-        let classToApply = slectedAnswer == currentQuestion.answer ? 'correct' :
-        'incorrect'
 
-        if(classToApply === 'correct') { 
+        let classToApply = slectedAnswer == currentQuestion.answer ? 'correct' :
+            'incorrect'
+
+        if (classToApply === 'correct') {
             score += SCORE_POINTS
             scoreText.innerText = score
             console.log(score)
@@ -165,7 +154,7 @@ choices.forEach(choice => {
 
         selectedChoice.parentElement.classList.add(classToApply)
 
-        setTimeout(() => { 
+        setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
         }, 1000)
